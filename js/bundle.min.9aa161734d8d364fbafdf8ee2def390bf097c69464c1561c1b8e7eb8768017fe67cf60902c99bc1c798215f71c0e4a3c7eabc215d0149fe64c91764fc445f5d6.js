a3ff7a2319926e07798fb4fa5659c86fe3515217
Erazem Kokot <contact@erazem.eu>
2019-09-10 15:39:56 +0200

    Publish to gh-pages (deploy.sh)

--- js/bundle.min.9aa161734d8d364fbafdf8ee2def390bf097c69464c1561c1b8e7eb8768017fe67cf60902c99bc1c798215f71c0e4a3c7eabc215d0149fe64c91764fc445f5d6.js
+++ js/bundle.min.9aa161734d8d364fbafdf8ee2def390bf097c69464c1561c1b8e7eb8768017fe67cf60902c99bc1c798215f71c0e4a3c7eabc215d0149fe64c91764fc445f5d6.js
@@ -1,2 +1,2 @@
 const header=document.querySelector("header");function paddingHeader(){document.body.style.paddingTop=`${header.offsetHeight}px`;}
-window.addEventListener("load",paddingHeader);;function cleanForm(){document.getElementById("contact-form").reset();}
+window.addEventListener("load",paddingHeader);function cleanForm(){document.getElementById("contact-form").reset();}
