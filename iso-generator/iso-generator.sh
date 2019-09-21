5b9cd2f2495592487487a5693e0b51eff126a12e
archer_dev <root@archer.sh>
2019-09-22 00:43:32 +0100

    Remove check and user input on aur packages

--- iso-generator/iso-generator.sh
+++ iso-generator/iso-generator.sh
@@ -149,13 +149,9 @@ local_repo_builds() { # prev: aur_builds
 
     # Begin build loop checking /tmp for existing builds, then build packages & install if required
     for pkg in $(echo "${local_aur_packages[@]}"); do
-        if [[ ! -d "/tmp/${pkg}" ]]; then
-            wget -qO- "${aur_snapshot_link}/${pkg}.tar.gz" | tar xz -C /tmp
-            cd /tmp/"${pkg}" || exit
-            case "${pkg}" in
-                perl-*|numix-*) makepkg -si --noconfirm ;;
-                *) makepkg -s ;;
-            esac
+        wget -qO- "${aur_snapshot_link}/${pkg}.tar.gz" | tar xz -C /tmp
+        cd /tmp/"${pkg}" || exit
+         makepkg -si --noconfirm --nocheck;;
         fi
     done
 
