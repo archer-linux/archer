2745424a79c9c1bc626ecdb54ae70d9d0052904e
archer_dev <root@archer.sh>
2019-09-22 01:59:22 +0100

    Add build_iso.sh in order to automatically build isos on any machine

--- iso-generator/build_iso.sh
+++ iso-generator/build_iso.sh
@@ -1,2 +1,8 @@
-docker build -t archer_builder .
-docker create --name archer-temp-container archer_builder && docker cp archer-temp-container:/home/archer-builder/archer-*.iso ./ && docker rm archer-temp-container
+docker build --no-cache -t archer_builder .
+docker create --name archer-temp-container archer_builder && docker cp archer-temp-container:/home/archer-builder/archer/archer-*.iso ./
+
+docker rm archer-temp-container
+docker rmi archer_builder
+
+echo "Done building Archer"
+ls -lah ./archer-*
